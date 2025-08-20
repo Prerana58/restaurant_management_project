8b528fe5c1165fb5714f6d6d4d7cc91b8f68feb2
Prerana58 <117636297+Prerana58@users.noreply.github.com>
2025-08-20 22:59:27 +0530

    32391

--- home/views.py
+++ home/views.py
@@ -3,3 +3,11 @@ from django.shortcuts import render
 # Create your views here.
 def about(request):
     return render(request, "home/about.html")
+
+def home(request):
+    restaurant=Restaurant.objects.first()
+    context={
+        "restaurant_name":
+        restaurant.name if restaurant else "Our Restaurant"
+    }
+    return render(request, "home/index.html", context)
