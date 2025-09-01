162f97b024508abeb3255e5f4e3bd8cfa32c1851
Prerana58 <117636297+Prerana58@users.noreply.github.com>
2025-09-01 18:54:59 +0530

    37949

--- home/views.py
+++ home/views.py
@@ -11,3 +11,16 @@ def index(request):
 def contact(request):
     restaurant_name=getattr(settings, "RESTAURANT_NAME", "My Restaurant")
     return render(request, "home/contact.html", {"restaurant_name":restaurant_name})
+
+def menu_view(request):
+    menu_items=[
+        {"name":"Margherita Pizza", "price":"250Rs",
+        "description":"Classic cheese pizza with tomato souce."},
+        {"name":"Paneer Tikka", "price":"300Rs", 
+        "description":"Grilled paneer cubes marinated in spices."},
+        {"name":"Veg Biryani", "price":"220Rs",
+        "description":"Aeomatic rice with vegetables and spices."},
+        {"name":"Pasta Alfredo", "price":"280Rs",
+        "description":"Creamy White Sauce pasta with herbs."},
+    ]
+    return render(request, "home/menu.html", {"menu_items":menu_items})
