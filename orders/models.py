342bcc13e9d1f4f1b207c57eec43816c1af3a418
Prerana58 <117636297+Prerana58@users.noreply.github.com>
2025-10-26 20:01:01 +0530

    56412

--- orders/models.py
+++ orders/models.py
@@ -3,6 +3,7 @@ from .models import OrderStatus
 
 # Create your models here.
 class Order(models.Model):
+    name=models.CharField(max_length=50,unique=True)
     status=models.ForeignKey('OrderStatus',on_delete=models.SET_NULL,null=True)
     def __str__(self):
         return f"Order #{self.id}-{self.status}"
