5d1f34b3d63392da8ab60858b68f0f10eb863e04
Prerana58 <117636297+Prerana58@users.noreply.github.com>
2025-10-26 22:47:11 +0530

    56557

--- home/models.py
+++ home/models.py
@@ -13,6 +13,16 @@ class MenuCategory(models.Model):
     def __str__(self):
         return self.name
 
+class MenuItem(models.Model):
+    name=models.CharField(max_length=100)
+    description=models.TextField(blank=True)
+    price=models.DecimalField(max_digits=8,decimal_places=2)
+    is_available=models.BooleanField(default=True)
+    is_featured=models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.name
+
 class DailySpecial(models.Model):
     name=models.CharField(max_length=100)
     description=models.TextField(blank=True)
