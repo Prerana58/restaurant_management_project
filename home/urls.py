1e3298fe01b52d02d6eab18b1e71f8adfa29a55d
Prerana58 <117636297+Prerana58@users.noreply.github.com>
2025-10-26 20:14:53 +0530

    56414

--- home/urls.py
+++ home/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     path('about/', views.about, name='about'),
     path('',views.home, name='home'),
     path("contact/", views.contact, name="contact"),
+    path('menu-categories/',MenuCategoryListView.as_view(),name='menu-categories'),
 ]
