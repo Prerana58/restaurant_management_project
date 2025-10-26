abac292d2e6845ed89292e0e03884c6723a0bf14
Prerana58 <117636297+Prerana58@users.noreply.github.com>
2025-10-26 22:49:24 +0530

    56557

--- home/serializers.py
+++ home/serializers.py
@@ -4,3 +4,8 @@ class MenuCategorySerializer(serializers.ModelSerializer):
     class Meta:
         model=MenuCategory
         fields=['name']
+
+class MenuItemSerializer(serializers.ModelSerializer):
+    class Meta:
+        model=MenuItem
+        fields=['id','name','description','price','is_available','is_featured']
