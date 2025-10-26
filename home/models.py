b6b09bf03f5d34d2d9e3d3331381d524cedb2753
Prerana58 <117636297+Prerana58@users.noreply.github.com>
2025-10-26 22:37:09 +0530

    56559

--- home/models.py
+++ home/models.py
@@ -12,3 +12,21 @@ class MenuCategory(models.Model):
     name=models.CharField(max_length=100,unique=True)
     def __str__(self):
         return self.name
+
+class DailySpecial(models.Model):
+    name=models.CharField(max_length=100)
+    description=models.TextField(blank=True)
+    price=models.DecimalField(max_digits=6,decimal_places=2)
+    available=models.BooleanField(default=True)
+    created_at=models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name
+
+    @staticmethod
+    def get_random_special():
+        specials=DailySpecial.objects.filter(available=True)
+        if not specials.exists():
+            return None
+
+        return specials.order_by('?').first()
