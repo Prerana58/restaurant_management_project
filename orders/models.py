a911923a90726e1191a7739ad45ae56a43aab3bd
Prerana58 <117636297+Prerana58@users.noreply.github.com>
2025-10-26 19:57:31 +0530

    56411

--- orders/models.py
+++ orders/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from .models import OrderStatus
 
 # Create your models here.
+class Order(models.Model):
+    status=models.ForeignKey('OrderStatus',on_delete=models.SET_NULL,null=True)
+    def __str__(self):
+        return f"Order #{self.id}-{self.status}"
+
+from orders.models import Order,OrderStatus
+status=OrderStatus.objects.create(name="Pending")
+order=Order.objects.create(status=status)
+print(order.status)
