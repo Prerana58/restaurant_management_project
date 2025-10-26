fa0a4db6081a975c7b1088a1bcf982ab043d9709
Prerana58 <117636297+Prerana58@users.noreply.github.com>
2025-10-26 19:46:33 +0530

    56410

--- home/models.py
+++ home/models.py
@@ -7,3 +7,8 @@ class Restaurant(models.Model):
 
     def __str__(self):
         return self.name
+
+class MenuCategory(models.Model):
+    name=models.CharField(max_length=100,unique=True)
+    def __str__(self):
+        return self.name
